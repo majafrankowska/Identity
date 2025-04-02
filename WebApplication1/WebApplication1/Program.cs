feaa4585be2b35edc6f9913103cd027b3ba976ae
Maja Frankowska <s24331@pjwstk.edu.pl>
2025-04-02 02:36:37 +0200

    appsettings update

--- WebApplication1/WebApplication1/Program.cs
+++ WebApplication1/WebApplication1/Program.cs
@@ -1,8 +1,20 @@
+using DotNetEnv;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+Env.Load(); 
 
+var rawConnection = builder.Configuration.GetConnectionString("DefaultConnection");
+
+var connectionString = Env.Expand(rawConnection);
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseNpgsql(connectionString));
+
+
+// Add services to the container.
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
